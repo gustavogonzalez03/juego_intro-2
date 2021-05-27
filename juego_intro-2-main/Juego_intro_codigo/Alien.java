f38631666da6bdd37b91f606a673aa28a22b5dd8
Gabyperezc <84479726+Gabyperezc@users.noreply.github.com>
2021-05-27 17:37:39 -0600

    Programa con sonidos

---
+++ juego_intro-2-main/Juego_intro_codigo/Alien.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Alien here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alien extends Actor
+{
+    SimpleTimer shortTimer = new SimpleTimer();
+    /**
+     * Act - do whatever the Alien wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if (shortTimer.millisElapsed() > 3000)
+        {
+            getWorld().addObject(new EnemyBullet(),getX(),getY());
+            Greenfoot.playSound("laser-01.wav");
+            shortTimer.mark();
+        }
+    }        
+    public void destroy()
+    {
+        Actor bullet;
+        bullet = getOneObjectAtOffset(0,0,Bullet.class);
+        if(bullet != null)
+        {
+            World world;
+            world = getWorld();            
+            world.removeObject(this); 
+        }
+    }
+}
